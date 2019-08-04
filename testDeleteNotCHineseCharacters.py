429bb47907c5a8fcc997a2dde86f66dd5b876d6d
RelativeWang <31396670+RelativeWang@users.noreply.github.com>
2019-08-04 18:19:58 +0800

    Add files via upload

---
+++ testDeleteNotCHineseCharacters.py
@@ -0,0 +1,17 @@
+# 删去非中文的内容
+
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("usage:python3 zhwiki.utf8.txt zhiwi.unseg.txt")
+        exit(1)
+    fout = open(sys.argv[2], encoding='utf8', mode='w')
+    with open(sys.argv[1], encoding = 'utf8') as fin:
+        for line in fin:
+            for word in line:
+                for char in word:
+                    if char == " ":
+                        fout.write(char)
+                    if char >= u'\u4e00' and char <= u'\u9fa5':
+                        fout.write(char)
